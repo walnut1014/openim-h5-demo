e6694d74cc91d9d02a668168a546f0cdd0811361
walnut <walnut1014@gmail.com>
2025-11-07 21:33:27 +0800

    update prod.env.ts

--- config/prod.env.ts
+++ config/prod.env.ts
@@ -1,4 +1,4 @@
-const BASE_DOMAIN = 'your-server-domain'
+const BASE_DOMAIN = 'chat.haxiag.com'
 const CHAT_URL = `http://${BASE_DOMAIN}/chat`
 const API_URL = `http://${BASE_DOMAIN}/api`
 const WS_URL = `ws://${BASE_DOMAIN}/msg_gateway`
